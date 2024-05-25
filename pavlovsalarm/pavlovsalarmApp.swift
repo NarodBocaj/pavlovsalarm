04ad73c0e62a7f5c9d56be768ff689fd38c2607e
NarodBocaj <jmdoran19@yahoo.com>
2024-05-25 18:38:26 -0500

    Initial Commit

---
+++ pavlovsalarm/pavlovsalarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  pavlovsalarmApp.swift
+//  pavlovsalarm
+//
+//  Created by Jacob Doran on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct pavlovsalarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
